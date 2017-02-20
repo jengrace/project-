98312edff1b96287447c1f0e7a91deee3a8a5686
Jennifer Grace <jenniferanngrace@gmail.com>
2017-02-20 06:05:45 +0000

    add cat breeds to seed data

--- server.py
+++ server.py
@@ -114,21 +114,24 @@ def add_animal_process():
     """ Sends admins form input to the database """
 
     admin = db.session.query(Admin.admin_id).filter(Admin.email == session['current_admin']).first()
+
     if request.method == 'POST':
         # Check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect('/admin/' + str(admin.admin_id))
+
         # Get the name of the uploaded file
         uploaded_file = request.files['file']
+
         # If user does not select a file, browser also
         # submits an empty part without filename
         if uploaded_file.filename == '':
             flash('No selected file')
             return redirect('/admin/' + str(admin.admin_id))
+
         # Check if the file is one of the allowed types/extensions
         if uploaded_file and allowed_file(uploaded_file.filename):
-
             name = request.form.get("name").title()
             gender = request.form.get("gender")
             age = request.form.get("age")
@@ -145,53 +148,60 @@ def add_animal_process():
             breed = db.session.query(Breed.breed_id).filter(Breed.breed_type == breed).first()
 
             rescue = db.session.query(Rescue).join(Admin).filter(Admin.email == session['current_admin']).first()
+
             user_filename = uploaded_file.filename
+
             # Store the extension of uploaded file to add to user_filename
             extension = user_filename.rsplit('.', 1)[1].lower()
-            # Get the next animal id to add to user_filename
-            result = db.session.query(func.max(Animal.animal_id)).one()
-            next_animal_id = str(result[0] + 1)
-            # Create file name based on rescue + animal ids
-            user_filename = str(rescue.rescue_id) + '-' + next_animal_id + '.' + extension
-            # Move the file from the temporal folder to the upload folder that was set up
-            path = os.path.join(app.config['UPLOAD_FOLDER'], user_filename)
-            uploaded_file.save(path)
 
-
-            # Creating an instance (row) in the animals table
-            animal = Animal(name=name, rescue=rescue, img_url=path,
+            animal = Animal(name=name, rescue=rescue,
                             gender_id=gender, age_id=age, size_id=size,
                             breed_id=breed, bio=bio, is_adopted=is_adopted,
                             is_visible=is_visible)
-            
+
             # Adding the animal instance to the animals table
             db.session.add(animal)
-            
+
+            db.session.commit()
+
+            a_id = animal.animal_id
+
+            user_filename = str(rescue.rescue_id) + '-' + str(a_id) + '.' + extension
+
+            path = os.path.join(app.config['UPLOAD_FOLDER'], user_filename)
+
+            uploaded_file.save(path)
+
+            animal.img_url = path
 
             db.session.commit()
 
     return redirect('/' + str(rescue.rescue_id))
+
 
 @app.route('/handle-add-rescue', methods=['GET', 'POST'])
 def add_rescue_process():
     """ Sends admins form input to the database """
 
     admin = db.session.query(Admin.admin_id).filter(Admin.email == session['current_admin']).first()
+
     if request.method == 'POST':
         # Check if the post request has the file part
         if 'file' not in request.files:
             flash('No file part')
             return redirect('/admin/' + str(admin.admin_id))
+
         # Get the name of the uploaded file
         uploaded_file = request.files['file']
+
         # If user does not select a file, browser also
         # submits an empty part without filename
         if uploaded_file.filename == '':
             flash('No selected file')
             return redirect('/admin/' + str(admin.admin_id))
+
         # Check if the file is one of the allowed types/extensions
         if uploaded_file and allowed_file(uploaded_file.filename):
-
             rescue_name = request.form.get("rescuename").title()
             phone = request.form.get("phone")
             address = request.form.get("address")
@@ -199,34 +209,62 @@ def add_rescue_process():
 
             #rescue = db.session.query(Rescue).join(Admin).filter(Admin.email == session['current_admin']).first()
             user_filename = uploaded_file.filename
+
             # Store the extension of uploaded file to add to user_filename
             extension = user_filename.rsplit('.', 1)[1].lower()
-            # Get the next animal id to add to user_filename
-            result = db.session.query(func.max(Rescue.rescue_id)).one()
-            next_rescue_id = str(result[0] + 1)
-            # Create file name based on rescue + animal ids
-            user_filename = next_rescue_id + '.' + extension
-            # Move the file from the temporal folder to the upload folder that was set up
-            path = os.path.join(app.config['UPLOAD_FOLDER'], user_filename)
-            uploaded_file.save(path)
 
-
-            # Creating an instance (row) in the rescues table
             rescue = Rescue(name=rescue_name, phone=phone, address=address,
-                            email=email, img_url=path)
+                            email=email)
 
-            # Adding the animal instance to the animals table
+            # Adding the rescue instance to the rescues table
             db.session.add(rescue)
 
             db.session.commit()
 
+            r_id = rescue.rescue_id
+
+            #Create file name based on the rescue's id
+            user_filename = str(r_id) + '.' + extension
+
+            # Move the file from the temporal folder to the upload folder that was set up
+            path = os.path.join(app.config['UPLOAD_FOLDER'], user_filename)
+
+            # Saving the file to the upload folder
+            uploaded_file.save(path)
+
+            # Updating the rescues table with rescue's image url
+            rescue.img_url = path
+
             admin = Admin.query.get(admin.admin_id)
-            admin.rescue_id = next_rescue_id
+
+            # Updating the admins table with the rescue's id
+            admin.rescue_id = r_id
 
             db.session.commit()
 
-    # TODO - redirect to a "successfully added rescue page with a button to take them either to homepage or add animal page"
-    return redirect('/')
+    return redirect('/success')
+
+
+@app.route('/success')
+def add_rescue_success():
+    """ Show login page for admins only. """
+
+    last_rescue_added = db.session.query(func.max(Rescue.rescue_id)).one()
+    last_rescue_added = last_rescue_added[0]
+
+    rescue_name = db.session.query(Rescue.name).filter(Rescue.rescue_id == last_rescue_added).one()
+    rescue_name = rescue_name[0]
+
+    last_admin_added = db.session.query(func.max(Admin.admin_id)).one()
+    last_admin_added = last_admin_added[0]
+
+    if 'current_admin' in session:
+        return render_template("success_rescue_add.html",
+                               last_rescue_added=last_rescue_added,
+                               last_admin_added=last_admin_added,
+                               rescue_name=rescue_name)
+    else:
+        return redirect('/')
 
 
 @app.route('/admin-login')
@@ -249,16 +287,13 @@ def process_admin_login():
         flash('Could not locate your account. Please click on sign up to create an account!')
         return redirect('/')
     if entered_password == admin.password:
-        print '****************** admin: **********', admin.rescue_id
         session['current_admin'] = entered_email
         ad_id = db.session.query(Admin.admin_id).filter(Admin.admin_id == admin.admin_id).one()
         ad_id = ad_id[0]
         flash('Logged in as %s' % entered_email)
 
-        # TODO if no rescue id- send to rescue add page
         if admin.rescue_id is None:
             return redirect('/admin' + '/rescue-info' + '/' + str(ad_id))
-        # TODO if rescue id- send to add animal page
         else:
             return redirect('/admin' + '/' + str(ad_id))
     else:
